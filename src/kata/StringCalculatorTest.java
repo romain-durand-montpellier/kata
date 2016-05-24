39a4311a9a829135be2bb9345b1dee0122632bf0
Romain D <romain.durand.montpellier@gmail.com>
2016-05-24 12:04:12 +0200

    Added 3 more tests

--- src/kata/StringCalculatorTest.java
+++ src/kata/StringCalculatorTest.java
@@ -18,5 +18,43 @@ public class StringCalculatorTest {
 		// THEN
 		assertEquals(0, somme);		
 	}
+	
+	@Test
+	public void StringCalculatorShouldReturnTheResultWithOnlyOneParam() {
 
+		// Given
+		StringCalculator s = new StringCalculator();
+
+		// WHEN
+		int somme = s.add("5");
+
+		// THEN
+		assertEquals(5, somme);		
+	}	
+
+	@Test
+	public void StringCalculatorShouldReturnTheResultWithTwoParam() {
+
+		// Given
+		StringCalculator s = new StringCalculator();
+
+		// WHEN
+		int somme = s.add("5,8");
+
+		// THEN
+		assertEquals(13, somme);		
+	}		
+
+	@Test
+	public void StringCalculatorShouldReturnTheResultWithSpacedParams() {
+
+		// Given
+		StringCalculator s = new StringCalculator();
+
+		// WHEN
+		int somme = s.add("5, 8");
+
+		// THEN
+		assertEquals(13, somme);		
+	}		
 }
